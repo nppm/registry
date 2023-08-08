650275a45d1cd410c68f3ec529081d93b3676392
沈赟杰 <shenyunjie@znrank.com>
2023-08-08 18:09:08 +0800

    update

--- src/controller/-/v1/login.c.ts
+++ src/controller/-/v1/login.c.ts
@@ -5,7 +5,7 @@ import { configs } from "../../../configs";
 export default defineController('POST', [
   NPMError(404)
 ], async req => {
-  if (!configs.value.authorization) throw new Error('系统使用默认登录方式');
+  if (!configs.value.authorization) return req.response().status(404);
   const body = req.getBody<{ create?: true, hostname: string }>();
   const { loginUrl, doneUrl } = await configs.value.authorization(body);
   const base64 = Buffer.from(doneUrl).toString('base64');
